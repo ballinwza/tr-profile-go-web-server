305a951421e3fcf188e0d3198dc31b15eb760944
Ball <ballgo120@gmail.com>
2025-01-07 02:01:41 +0700

    feat: 🎸 update aggreagtion pipeline for filter data

--- handler/lotto/getAllLotto.go
+++ handler/lotto/getAllLotto.go
@@ -3,17 +3,57 @@ package handler_lotto
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	struct_lotto "github.com/render-examples/go-gin-web-server/struct/lotto"
 	"go.mongodb.org/mongo-driver/v2/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func (m *LottoService) GetAllLottoHandler(c *gin.Context) {
 	var lotties []struct_lotto.Lotto
-	opts := options.Find().SetSort(bson.M{"date": -1}) // DESC
-	cursor, err := m.collection.Find(context.TODO(), bson.M{}, opts)
+	var req struct_lotto.LottoFilterReq
+	err := c.ShouldBind(&req)
+	filter := bson.M{}
+
+	if req.First != nil {
+		filter["first_prize"] = req.First
+	}
+	// if req.BackThreeDigit != nil {
+	// 	filter["back_three_digit_prize"] = req.BackThreeDigit
+	// }
+	if req.BackTwoDigit != nil {
+		filter["back_two_digit_prize"] = req.BackTwoDigit
+	}
+	// if req.FrontThreeDigit != nil {
+	// 	filter["front_three_digit_prize"] = req.FrontThreeDigit
+	// }
+	if req.Year != nil {
+		yearFromDate, err := strconv.Atoi(*req.Year)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.Error{
+				Err:  err,
+				Type: http.StatusBadRequest,
+				Meta: err.Error(),
+			})
+			return
+		}
+
+		filter["$expr"] = bson.M{
+			"$eq": []interface{}{
+				bson.M{"$year": "$date"},
+				yearFromDate,
+			},
+		}
+	}
+
+	pipeline := mongo.Pipeline{
+		{{Key: "$match", Value: filter}},
+		{{Key: "$sort", Value: bson.M{"date": -1}}},
+	}
+
+	cursor, err := m.collection.Aggregate(context.TODO(), pipeline)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.Error{
 			Err:  err,
@@ -33,7 +73,6 @@ func (m *LottoService) GetAllLottoHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"data":   lotties,
+		"data": lotties,
 	})
 }
---
+++ struct/lotto/lotto.req.go
@@ -0,0 +1,9 @@
+package struct_lotto
+
+type LottoFilterReq struct {
+	Year            *string   `form:"year"`
+	BackThreeDigit  *[]string `form:"b3d"`
+	BackTwoDigit    *string   `form:"b2d"`
+	First           *string   `form:"first"`
+	FrontThreeDigit *[]string `form:"f3d"`
+}
