a6d0659f15dc7de96d53631c6fc2fd20ac31f39a
Tradon <tradon@praneat.com>
2025-01-10 22:24:42 +0700

    feat: 🎸 implement book services

--- services/lotto/lotto.response.go
+++ services/lotto/lotto.response.go
@@ -1,10 +1,10 @@
 package services_lotto
 
-import struct_lotto "github.com/render-examples/go-gin-web-server/models/lotto"
+import models_lotto "github.com/render-examples/go-gin-web-server/models/lotto"
 
 type responseLottoList struct {
-	Data []struct_lotto.Lotto `json:"data"`
+	Data []models_lotto.Lotto `json:"data"`
 }
 type responseLotto struct {
-	Data struct_lotto.Lotto `json:"data"`
+	Data models_lotto.Lotto `json:"data"`
 }
